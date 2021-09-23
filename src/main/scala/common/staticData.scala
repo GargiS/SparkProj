1e0648876dc68274e00216283faeb77e566cd456
sanug <prsharma1308@gmail.com>
2021-09-23 15:31:42 +0530

    10th

--- src/main/scala/common/staticData.scala
+++ src/main/scala/common/staticData.scala
@@ -4,8 +4,7 @@ object staticData {
 
   /** Defining all static parameters */
 
-  //val outFilePath: String = "/output"
-  val outFilePath: String = "/Users/gargi/Documents/InterviewQuestions/SparkScalaTest/output"
+  val outFilePath: String = "/output"
   val factFilePath: String = "/fact.csv"
   val lookUpFilePath: String = "/lookup.csv"
 }
